8f2b1697af88c6645cd75c97e8e35820bf90b59a
akramnarejo <ak.narejo@gmail.com>
2023-06-23 07:56:17 +0500

    fix: blog ui

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,21 +2,22 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import useStore from "../store";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 async function getPosts() {
   const res = await fetch(
     `https://cms.cloudmeshsolutions.com/api/blog?filter={"websiteAdmin":"646ce566317ec72916785a0c"}`
   );
   const posts = await res.json();
-  console.log(posts)
+  console.log(posts);
   return posts;
 }
 const Page = () => {
   const router = useRouter();
   const userInfo = useStore((state) => state?.userInfo ?? {});
   console.log("user info: ", userInfo);
-  const [posts, setPosts] = useState({})
-  
+  const [posts, setPosts] = useState({});
+
   useEffect(() => {
     const fetchData = async () => {
       if (userInfo?.email) {
@@ -29,21 +30,29 @@ const Page = () => {
     fetchData();
   }, [userInfo]);
 
-
   return (
     <div className="pt-10 flex flex-col gap-8">
       {posts?.data
         ? posts?.data.Blog.map((post, index) => {
             return (
-              <div key={index} className="flex flex-col gap-2">
-                <Link
-                  href={`/blog/post/${post.id}?pagePath=${post.pagePath}`}
-                  className="text-2xl hover:text-gray-500"
-                >
-                  {post.title.en}
-                </Link>
-                <p>{post.description.en}</p>
-                <p className="text-sm text-gray-500 pb-2">- {post.author}</p>
+              <div key={index} className="flex gap-4">
+                <Image
+                  src={post?.thumbnail?.en}
+                  alt={post?.title}
+                  width={200}
+                  height={250}
+                  className="rounded-md object-cover"
+                />
+                <div className="flex flex-col gap-2">
+                  <p className="text-sm text-gray-500">- {post.author}</p>
+                  <Link
+                    href={`/blog/post/${post.id}?pagePath=${post.pagePath}`}
+                    className="text-2xl hover:text-gray-500"
+                  >
+                    {post.title.en}
+                  </Link>
+                  <p className="text-sm">{post.description.en}</p>
+                </div>
               </div>
             );
           })
